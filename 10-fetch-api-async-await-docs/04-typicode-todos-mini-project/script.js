2961d5b5601b610902e2f3193be9af0b89529c49
xtrasee <thn018@ucsd.edu>
2023-10-23 23:35:34 -0700

    todo miniproj part 2

--- 10-fetch-api-async-await-docs/04-typicode-todos-mini-project/script.js
+++ 10-fetch-api-async-await-docs/04-typicode-todos-mini-project/script.js
@@ -1,47 +1,77 @@
 const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
 
-const getTodos = () => {
-    fetch(apiUrl + '?_limit=5')
-        .then((res) => res.json())
-        .then((data) => {
-            data.forEach((todo) => addTodoToDOM(todo));
-        });
-};
-
-const addTodoToDOM = (todo) => {
-    const div = document.createElement('div');
-    div.appendChild(document.createTextNode(todo.title));
-    div.setAttribute('data-id', todo.id);
-
-    if (todo.completed) {
-        div.classList.add('done');
-    }
-
-    document.getElementById('todo-list').appendChild(div);
+function getTodos() {
+  fetch(`${apiUrl}?_limit=5`)
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((todo) => addTodoToDOM(todo));
+    });
 }
 
-const createTodo = (e) => {
-    e.preventDefault();
+function createTodo(e) {
+  e.preventDefault();
 
-    const newTodo = {
-        title: e.target.firstElementChild.value,
-        completed: false,
-    };
-
-    fetch(apiUrl, {
-        method: 'POST',
-        body: JSON.stringify(newTodo),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-        .then((res) => res.json())
-        .then((data) => addTodoToDOM(data))
-};
-
-const init = () => {
-    document.addEventListen('DOMContentLoaded', getTodos);
-    document.querySelector('#todo-form').addEventListener('submit', createTodo);
+  const newTodo = {
+    title: e.target.firstElementChild.value,
+    completed: false,
+  };
+  fetch(apiUrl, {
+    method: 'POST',
+    body: JSON.stringify(newTodo),
+    headers: {
+      'Content-Type': 'application/json',
+      token: 'abc123',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => addTodoToDOM(data));
 }
 
-getTodos();
+function addTodoToDOM(todo) {
+  const div = document.createElement('div');
+  div.appendChild(document.createTextNode(todo.title));
+  div.setAttribute('data-id', todo.id);
+
+  if (todo.completed) {
+    div.classList.add('done');
+  }
+
+  document.getElementById('todo-list').appendChild(div);
+}
+
+function toggleComplete(e) {
+  e.target.classList.toggle('done');
+  updateTodo(e.target.dataset.id, e.target.classList.contains('done'));
+}
+
+function updateTodo(id, completed) {
+  fetch(`${apiUrl}/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({ completed }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+}
+
+function deleteTodo(e) {
+  const id = e.target.dataset.id;
+  fetch(`${apiUrl}/${id}`, {
+    method: 'DELETE',
+  })
+    .then((res) => res.json())
+    .then(() => e.target.remove());
+}
+
+function init() {
+  document.addEventListener('DOMContentLoaded', getTodos);
+  document.getElementById('todo-form').addEventListener('submit', createTodo);
+  document
+    .getElementById('todo-list')
+    .addEventListener('click', toggleComplete);
+  document.getElementById('todo-list').addEventListener('dblclick', deleteTodo);
+}
+
+init();
